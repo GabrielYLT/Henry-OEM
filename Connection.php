59d987c61823b9e99aec8d94c6324d81f985c055
GabrielYLT <90229401+GabrielYLT@users.noreply.github.com>
2022-07-14 10:02:02 +0800

    1

---
+++ Connection.php
@@ -0,0 +1,16 @@
+
+<?php
+
+$connect = mysqli_connect("localhost","root","","oem");
+
+if(!$connect)
+{
+		?>
+		<script type="text/javascript">
+		alert("Server Down");
+		</script>
+		<?php
+	echo("Connect Unsuccessfully");
+}
+
+?>
